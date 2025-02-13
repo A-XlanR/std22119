c71f3d29cb9465e30f6847536019630126f3d924
A-XlanR <ainadylanrasamoelina@gmail.com>
2025-02-13 08:13:27 +0300

    fix: typo in import

--- src/main/java/com/xlan/demo/endpoint/rest/controller/health/PingController.java
+++ src/main/java/com/xlan/demo/endpoint/rest/controller/health/PingController.java
@@ -4,7 +4,7 @@ import com.xlan.demo.PojaGenerated;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;ep
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @PojaGenerated
